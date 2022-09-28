2306fac33fd8ff27cfea8859d31642424e3d3cd0
nicol <nicol@DESKTOP-1186UFK>
2022-09-28 18:10:10 +0200

    sysout od nicol

--- src/sk/upjs/paz1C/Alexandra.java
+++ src/sk/upjs/paz1C/Alexandra.java
@@ -5,6 +5,7 @@ public class Alexandra {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ahoj");
+		System.out.println("Nicol tu bola B) ");
 
 	}
 
